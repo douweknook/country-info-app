8f41f764e8d51b967570316ffad679d57448e06f
douweknook <douweknook@gmail.com>
2016-10-11 17:39:08 +0200

    did part 2

--- app2.js
+++ app2.js
@@ -4,10 +4,8 @@ var reader = require(__dirname+'/modules/json-file-reader')
 function findData(data) {
 	for (var i = 0; i < data.length; i++) {
 		if (data[i].name == process.argv[2]) {
-			// Country: <name>
-			console.log("Country: "+data[i].name)
-			// Top Level Domain: <topLevelDomain>
-			console.log("Top Level Domain: "+data[i].topLevelDomain)
+			// Country: <name> Top Level Domain: <topLevelDomain>
+			console.log("Country: "+data[i].name+"\nTop Level Domain: "+data[i].topLevelDomain)
 		}
 	}
 }
