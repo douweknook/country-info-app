18d39c6b536e8275ad62684bfb2b7d68e7395266
douweknook <douweknook@gmail.com>
2016-10-11 15:41:15 +0200

    found a first solution

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var fs = require('fs')
 
-
 // Read and parse data from JSON file
-var getData = function(filename) {
-
+var getData = function(filename, countryName) {
+	// Read data file in as string
+	fs.readFile(filename, 'utf-8', function(err, data) {
+		if (err) {
+			throw err
+		}
+		// Parse data into JS object 
+		countriesData = JSON.parse(data)
+		// Go over data to find required country info
+		for (var i = 0; i < countriesData.length; i++) {
+			if (countriesData[i].name == countryName) {
+				// Country: <name>
+				console.log("Country: "+countriesData[i].name)
+				// Top Level Domain: <topLevelDomain>
+				console.log("Top Level Domain: "+countriesData[i].topLevelDomain)
+			}
+		}
+	})
 }
+
+getData("countries.json", process.argv[2])
