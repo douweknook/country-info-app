7794de928a98c9912a250c9845dfa74228a151aa
douweknook <douweknook@gmail.com>
2016-10-12 14:32:32 +0200

    changed file names

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-// SOLUTION FOR PART ONE
+// SOLUTION FOR PART TWO
 
-var fs = require('fs')
-var fileReader = require(__dirname+'/modules/json-file-reader')
+// Require functions from module
+var reader = require(__dirname+'/modules/json-file-reader')
 
-// Read and parse data from JSON file
-var getData = function(filename) {
-	// Read data file in as string
-	fs.readFile(filename, 'utf-8', function(err, data) {
-		if (err) {
-			throw err
-		}
-		// Parse data into JS object 
-		countriesData = JSON.parse(data)
-		// Go over data to find required country info
-		for (var i = 0; i < countriesData.length; i++) {
-			if (countriesData[i].name == process.argv[2]) {
-				// Country: <name>
-				console.log("Country: "+countriesData[i].name)
-				// Top Level Domain: <topLevelDomain>
-				console.log("Top Level Domain: "+countriesData[i].topLevelDomain)
-			}
-		}
-	})
-}
-
-getData(__dirname+"/countries.json")
+// Start app
+console.log("Let me search for info on "+process.argv[2]+"...")
+reader.filereader(__dirname+'/countries.json', reader.filter)
--- app2.js
+++
@@ -1,8 +0,0 @@
-// SOLUTION FOR PART TWO
-
-// Require functions from module
-var reader = require(__dirname+'/modules/json-file-reader')
-
-// Start app
-console.log("Let me search for info on "+process.argv[2]+"...")
-reader.filereader(__dirname+'/countries.json', reader.filter)
