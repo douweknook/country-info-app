fceae25509b5d71600549806d24b1e9a8fdece44
douweknook <douweknook@gmail.com>
2016-10-12 14:32:51 +0200

    changed file names

---
+++ app_old.js
@@ -0,0 +1,38 @@
+// THIS CODE DOES NOT FULLY WORK. FS.READFILE FUNCTION OVERWRITES THE WHOLE this. THING.
+
+var fs = require('fs')
+
+var Reader = function(source){
+	this.country = process.argv[2],
+	this.result = {},
+	this.read = function(){
+		console.log('reading file')
+		fs.readFile(source, 'utf-8', function(err, data) {
+			if (err) throw err
+			// Parse data into JS object 
+			this.json = JSON.parse(data)
+			// Go over data to find required country info
+			console.log(this.result)
+		})
+	},
+	this.filter = function(){
+		console.log('filtering file')
+		for (var i = 0; i < this.json.length; i++) {
+				if (this.json[i].name == process.argv[2]) {
+					// Country: <name>
+					this.result.country = this.json[i].name
+					console.log(this.json[i].name)
+					// Top Level Domain: <topLevelDomain>
+					this.result.tld = this.json[i].topLevelDomain
+					console.log(this.json[i].topLevelDomain)
+				}
+			}
+	}
+	this.print = function(){
+		console.log(this.result.country)
+		console.log(this.result.tld)
+	}
+}
+
+var result = new Reader(__dirname+'/countries.json')
+result.read()
